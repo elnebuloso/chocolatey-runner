6d62a37695b6bfce2d65fc81f02872f7aaae1e86
Jan Thoennessen <jan.thoennessen@cbc.de>
2019-12-12 08:56:03 +0100

    ## [2.0.0] - 2019-09-11 - installing packages via tag

--- setup.ps1
+++ setup.ps1
@@ -11,12 +11,13 @@ if (!(Test-Path "$pwd/packages.json"))
 }
 
 $packages = Get-Content -Raw -Path "$pwd/packages.json" | ConvertFrom-Json | Select-Object -expand packages
+$packagesToDelete = @()
 
 foreach ($package in $packages)
 {
     if ($package.tags.count -eq 0 -or $package.tags -notcontains $tag)
     {
-        choco uninstall -y $package.name
+        $packagesToDelete += $package.name
     }
 
     if ($package.tags.count -gt 0 -and $package.tags -contains $tag)
@@ -24,3 +25,6 @@ foreach ($package in $packages)
         choco upgrade -y $package.name $package.opts
     }
 }
+
+$packagesToDelete = $packagesToDelete | Sort-Object
+choco uninstall -y $packagesToDelete
